deb74b6c838412c87d56de051c720d7322552bd3
timosarkar <timosarkartimo@gmail.com>
2025-01-16 16:40:23 +0100

    Update decompressedsize.py

--- decompressedsize.py
+++ decompressedsize.py
@@ -1,4 +1,3 @@
-
 def main():
   init = <your number>
   gb = init / 1024
@@ -10,34 +9,7 @@ def main():
   bb = yb / 1024
   gp = bb / 1024
   sb = gp / 1024
-  ab = sb / 1024
-  kb = ab / 1024
-  amosbyte = kb / 1024
-  pectrobyte = amosbyte / 1024
-  bolgerbyte = pectrobyte / 1024
-  sambobyte = bolgerbyte / 1024 # we are here (v1)
-  quesabyte = sambobyte / 1024
-  kinsabyte = quesabyte / 1024
-  rutherbyte = kinsabyte / 1024
-  dubnibyte = rutherbyte / 1024 # (v2)
-  seaborgbyte = dubnibyte / 1024
-  bohrbyte = seaborgbyte / 1024 # (v3)
-  hassiubyte = bohrbyte / 1024
-  meitnerbyte = hassiubyte / 1024
-  darmstadbyte = meitnerbyte / 1024
-  roentbyte = darmstadbyte / 1024
-  sophobyte = roentbyte / 1024
-  coperbyte = sophobyte / 1024 # (v4)
-  koentekbyte = coperbyte / 1024
-  glossybyte = koentekbyte / 1024
-  snugbyte = glossybyte / 1024
-  grandbyte = snugbyte / 1024
-  doombyte = grandbyte / 1024
-  godbyte = doombyte / 1024
-  superbyte = godbyte / 1024 # (v5)
-  googolbyte = superbyte / 1024
-  primebyte = googolbyte / 100000000000000000000000000000000000000000000000000000000000 # new unit | which is 1 googolbyte * 10^59
-  print(primebyte)
+  print(sb)
 
 if __name__ == "__main__":
     main()
